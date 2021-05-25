fa845a03ef45e3160da38623a3382980eedbb6a3
GellenLiu <hfut2018214813@163.com>
2021-05-25 23:06:30 +0800

    导航栏完成，首页完成部分

--- font/src/App.vue
+++ font/src/App.vue
@@ -1,16 +1,41 @@
 <template>
   <div id="app">
-    <index></index>  
+	  <div class="navigation-bar">
+	  	<div class="tabbar" :class="{'active': tab==0}" @click="switchtab(0)">首页</div>
+	  	<div class="tabbar" :class="{'active': tab==1}" @click="switchtab(1)">教育经历</div>
+	  	<div class="tabbar" :class="{'active': tab==2}" @click="switchtab(2)">项目经历</div>
+	  	<div class="tabbar" :class="{'active': tab==3}" @click="switchtab(3)">个人博客</div>
+	  	<div class="tabbar" :class="{'active': tab==4}" @click="switchtab(4)">个人愿景</div>
+	  </div>
+      <!-- 路由匹配到的组件将渲染在这里 -->
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
-import index from './pages/index/index.vue'
-
 export default {
   name: 'app',
-  components: {
-    index
+  data(){
+	  return{
+        tab: 0
+	  }
+  },
+  methods:{
+	  switchtab(tab){
+		 this.tab = tab
+		 if(tab==0){
+			this.$router.push('/')
+		 }
+		 if(tab==1){
+		 	this.$router.push('/education')
+		 }
+		 if(tab==2){
+		 	this.$router.push('/')
+		 }
+		 if(tab==3){
+		 	this.$router.push('/')
+		 }
+	  }
   }
 }
 </script>
@@ -27,9 +52,42 @@ export default {
   margin: 0;
   padding: 0;
 }
+
 html,body{
   height: 100%;
   width: 100%;
   margin: 0;
+  white-space: nowrap; 
 }
+
+
+.navigation-bar{
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  height: 60px;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.tabbar{
+  height: 50px;
+  width: 200px;
+  margin: 10px 0 0 0;
+  font-size: 25px;
+  text-decoration: blink;
+  line-height: 50px;
+  color: #FFFFFF;
+}
+
+.tabbar:hover{
+  background-color: rgba(255, 179, 55, 0.5);
+}
+
+.active{
+  color: rgb(255,179,55);
+  border-bottom: 2px solid #ffaa00;
+}
+
 </style>
--- font/src/pages/index/index.vue
+++ font/src/pages/index/index.vue
@@ -1,23 +1,36 @@
 <template>
 	<div class="page">
-		<div class="navigation-bar">
-			<div class="tabbar active">首页</div>
-			<div class="tabbar">教育经历</div>
-			<div class="tabbar">项目经历</div>
-			<div class="tabbar">个人博客</div>
-			<div class="tabbar">愿景</div>
+		<div class="content">
+			<div class="title">HI , I AM Gellen Liu</div>
+		    <div class="sub-title">welcome to my world</div>
+		    <div class="link" @click="more">learn more</div>
 		</div>
-		<div class="title">Hello , I AM Gellen Liu</div>
-		<div class="sub-title">welcome to my world</div>
-		<div class="link" @click="more">learn more</div>
+		<div class="img-box">
+			<!-- <img /> -->
+		</div>
+		
 	</div>
 </template>
 
 <script>
+export default {
+	data(){
+		return {
+			
+		}
+	},
+	methods:{
+		more(){
+			console.log("more")
+		    this.$router.push('/education')
+		}
+	}
+}
 </script>
 
 <style scoped>
 .page{
+  display: flex;	
   height: 100%;
   width: 100%;
   background-size: cover;
@@ -26,48 +39,29 @@
   overflow: hidden;
 }
 
-.navigation-bar{
+.content{
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  position: fixed;
-  top: 0;
-  height: 60px;
-  width: 100%;
-  margin: 0 auto;
-}
-
-.tabbar{
-  height: 50px;
-  width: 200px;
-  margin: 10px 0 0 0;
-  border-radius: 10px;
-  font-size: 25px;
-  line-height: 50px;
-  color: #FFFFFF;
-}
-
-.tabbar:hover{
-  background-color: rgba(255, 179, 55, 0.5);
-}
-
-.active{
-  color: rgb(255,179,55);
-  border-bottom: 2px solid #ffaa00;
+  align-items: flex-start;
+  height: 600px;
+  width: 600px;	
+  
 }
 
 .title{
-  height: 100px;
-  margin: 300px 0 0 100px;
+  height: 80px;
+  margin: 30px 0 0 100px;
   font-family: "arial rounded mt bold";
-  font-size: 50px;
+  font-size: 60px;
   color: white;
   text-align: left;
 }
 
 .sub-title{
   height: 50px;
-  margin: 10px 0 0 100px;
-  font-size: 25px;
+  margin: 0 0 0 100px;
+  font-size: 30px;
   opacity: 0.5;
   text-align: left;
   color: white;
@@ -79,5 +73,10 @@
   font-size: 30px;
   color: #fff;
   text-align: left;
+}
+
+.img-box{
+  height: 600px;
+  width: 300px;	
 }
 </style>
